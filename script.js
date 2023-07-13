b4b31f085a83772805b6ad391bf354f906ce5cb5
Shajid <shajid1biz@gmail.com>
2023-07-13 21:54:10 +0530

    Added the event listeners to the color 1 and color 2 input that changes the color

---
+++ script.js
@@ -0,0 +1,17 @@
+let css = document.querySelector('h3');
+let color1 = document.querySelector('.color1');
+let color2 = document.querySelector('.color2');
+let body = document.getElementById('gradient');
+
+// console.log(css);
+// console.log(color1);
+// console.log(color2);
+
+color1.addEventListener("input", function() {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+})
+
+color2.addEventListener("input", function() {
+    body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+})
+
